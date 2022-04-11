e470697b882d9219afc99ef05cd4d78741f2e774
Sheila Strahan <smstrahan@gmail.com>
2022-04-11 10:09:30 -0600

    workshop5, task 2

--- workshop5.py
+++ workshop5.py
@@ -20,4 +20,23 @@ def guess_random_number(tries, start, stop):
     print("You have failed to guess the number:", random_number)
 
 
-guess_random_number(5, 0, 10)
+def guess_random_num_linear(tries, start, stop):
+    random_number = random.randint(start, stop+1)
+    print("The number for the program to guess is:", random_number)
+    for guess in range(start, stop+1):
+        print("Number of tries left:", tries)
+        print("The program is guessing", guess)
+        if guess == random_number:
+            print("The program has guessed the correct number!")
+            return
+        tries -= 1
+        if tries <= 0:
+            print("The program has failed to guess the correct number.")
+            return
+
+
+# Driver Code Task 1
+# guess_random_number(5, 0, 10)
+
+# Driver Code Task 2
+guess_random_num_linear(5, 0, 10)
